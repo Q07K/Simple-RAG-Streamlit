a7f12892008f04ce631376e17c161c0743098780
gyuhyeong <kgh0730@gmail.com>
2024-12-13 01:52:45 +0900

    Feat: Zilliz 연결 및 embedding 모델 수정

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_milvus import Milvus
+from langchain_milvus import Zilliz
 
 
 def embedding_model() -> GoogleGenerativeAIEmbeddings:
@@ -11,39 +11,34 @@ def embedding_model() -> GoogleGenerativeAIEmbeddings:
     -------
     GoogleGenerativeAIEmbeddings
         불러온 Embedding 모델
-
-    Model list
-    ----------
-    - "models/embedding-001"
-    -
     """
     return GoogleGenerativeAIEmbeddings(
-        model="models/embedding-001",
+        model="models/text-embedding-004",
         google_api_key=st.secrets["api_key"],
     )
 
 
-def milvus_client() -> Milvus:
-    return Milvus(
+def zilliz_client() -> Zilliz:
+    return Zilliz(
         embedding_function=embedding_model(),
-        collection_name=st.secrets["collection_name"],
         connection_args={
             "uri": st.secrets["milvus_uri"],
             "token": st.secrets["milvus_token"],
         },
+        collection_name=st.secrets["collection_name"],
         auto_id=True,
     )
 
 
-def pdf_extractor(path):
+def pdf_extractor(path):  # -> list:
     pages = []
     for page in PyPDFLoader(file_path=path).load():
         pages.append(page)
     return pages
 
 
-def add_pdf(byte_file):
-    vector_db = milvus_client()
+def add_pdf(byte_file) -> None:
+    vector_db = zilliz_client()
     with open(byte_file.name, mode="wb") as w:
         w.write(byte_file.getvalue())
     vector_db.add_documents(documents=pdf_extractor(byte_file.name))
